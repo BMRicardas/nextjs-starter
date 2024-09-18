3ef5ec206b6f9d75b507f145e99fe8600333bd4b
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-09-19 00:57:22 +0300

    add and setup Next UI

---
+++ src/components/providers.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+import { NextUIProvider } from "@nextui-org/react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export function Providers({ children }: Props) {
+  return <NextUIProvider>{children}</NextUIProvider>;
+}
