5db11274b72de8ea9ce4a57a06ff2d1a571634f2
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-09-20 00:52:00 +0300

    refactor profile page and add requireAuth utility

---
+++ src/utils/require-auth.ts
@@ -0,0 +1,13 @@
+import { redirect } from "next/navigation";
+
+import { getServerSession } from "next-auth";
+
+import { options } from "@/config/auth";
+
+export async function requireAuth() {
+  const session = await getServerSession(options);
+
+  if (!session?.user) {
+    redirect("/");
+  }
+}
