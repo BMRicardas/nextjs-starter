c64d6c12338b01df9a0517ec9ad0503b9cd45f62
BMRicardas <Ricardas.Brazdzius@gmail.com>
2024-09-20 02:01:04 +0300

    refactor guestbook entries schema and add user relation

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,4 +1,7 @@
 export { accounts } from "./accounts";
-export { guestbookEntries } from "./guestbook-entries";
+export {
+  guestbookEntries,
+  guestbookEntriesRelations,
+} from "./guestbook-entries";
 export { sessions } from "./sessions";
 export { users } from "./users";
